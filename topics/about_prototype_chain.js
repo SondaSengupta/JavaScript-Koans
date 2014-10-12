e57774ad98cc9f35f887bdf28ce7f0efecb35b60
SondaSengupta <sondasengupta@gmail.com>
2014-10-11 20:04:04 -0500

    Finished 56th koan with comments

--- topics/about_prototype_chain.js
+++ topics/about_prototype_chain.js
@@ -25,39 +25,42 @@ child.b = 2;
  * [null]
  * ---------------------- ---- ---- ----
  * */
-
+//Question 51 asks whether the variable child has an a and b property. [true]//
 test("Is there an 'a' and 'b' own property on child?", function () {
-  equal(__, child.hasOwnProperty('a'), 'child.hasOwnProperty(\'a\')?');
-  equal(__, child.hasOwnProperty('b'), 'child.hasOwnProperty(\'b\')?');
+  equal(true, child.hasOwnProperty('a'), 'child.hasOwnProperty(\'a\')?');
+  equal(true, child.hasOwnProperty('b'), 'child.hasOwnProperty(\'b\')?');
 });
 
+//Question 52 asks what a and b are in child//
 test("Is there an 'a' and 'b' property on child?", function () {
-  equal(__, child.a, 'what is \'a\' value?');
-  equal(__, child.b, 'what is \'b\' value?');
+  equal(1, child.a, 'what is \'a\' value?');
+  equal(2, child.b, 'what is \'b\' value?');
 });
 
+//Question 53 states that if the b vlue in child was removed what would be the b value now. In this case, it would be the father's b value which is [3]//
 test("If 'b' was removed, whats b value?", function () {
   delete child.b;
-  equal(__, child.b, 'what is \'b\' value now?');
+  equal(3, child.b, 'what is \'b\' value now?');
 });
 
-
+//Question 54 asks whether the child variable contains a c value? [false]//
 test("Is there a 'c' own property on child?", function () {
-  equal(__, child.hasOwnProperty('c'), 'child.hasOwnProperty(\'c\')?');
+  equal(false, child.hasOwnProperty('c'), 'child.hasOwnProperty(\'c\')?');
 });
 
+//Question 55 asks about prototype. A prototype is located outside of the function and it alllows you to make all functions complete what is in the prototype without having it copied within each function. The prototype states that c is 4, so running the prototype with child gives [4]//
 // Is there a 'c' own property on child? No, check its prototype
 // Is there a 'c' own property on child.[[Prototype]]? Yes, its value is...
 test("Is there a 'c' property on child?", function () {
-  equal(__, child.c, 'what is the value of child.c?');
+  equal(4, child.c, 'what is the value of child.c?');
 });
 
-
+//Question 56 asks about a prototype property that does not exist. What will be the output if you ask for prototype property that does not exist. It is undefined.//
 // Is there a 'd' own property on child? No, check its prototype
 // Is there a 'd' own property on child.[[Prototype]]? No, check it prototype
 // child.[[Prototype]].[[Prototype]] is null, stop searching, no property found, return...
 test("Is there an 'd' property on child?", function () {
-  equal(__, child.d, 'what is the value of child.d?');
+  equal(undefined, child.d, 'what is the value of child.d?');
 });
 
 
