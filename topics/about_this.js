c7c722e111f65859f8a589a9c2a240c481f8cbb0
SondaSengupta <sondasengupta@gmail.com>
2014-10-13 16:24:28 -0500

    Finished the 69th koan in about_this.js

--- topics/about_this.js
+++ topics/about_this.js
@@ -1,15 +1,17 @@
 module("About this (topics/about_this.js)");
 
+//Question 67 that tells you that you can access properties using the this function even within a method//
 test("'this' inside a method", function () {
 	var person = {
 		name: 'bob',
 		intro: function () {
-			return "Hello, my name is " + this.__;
+			return "Hello, my name is " + this.name;
 		} 
 	}
 	equal(person.intro(), "Hello, my name is bob", "If an object has a method can you access properties inside it?");
 });
 
+//Question 68 in which globalName is changed to Peter// 
 test("'this' on unattached function", function () {
 	var person = {
 		globalName: 'bob',
@@ -22,10 +24,11 @@ test("'this' on unattached function", function () {
 	
 	// if the function is not called as an object property 'this' is the global context 
 	// (window in a browser). This is an example. Please do not do this in practise.
-	window.__ = 'Peter';
+	window.globalName = 'Peter';
 	equal(alias(), "Hello, my name is Peter", "What does 'this' refer to when it is not part of an object?");
 });
 
+//Question 69//
 test("'this' set explicitly", function () {
 	var person = {
 		name: 'bob',
@@ -35,7 +38,7 @@ test("'this' set explicitly", function () {
 	}
 
 	// calling a function with 'call' lets us assign 'this' explicitly
-	var message = person.intro.call({__: "Frank"});
+	var message = person.intro.call({name: "Frank"});
 	equal(message, "Hello, my name is Frank", "What does 'this' refer to when you use the 'call()' method?");
 });
 
